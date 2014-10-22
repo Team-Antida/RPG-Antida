317b0a2becb55902e2534fc81f0dae6f1b510a1f
ayatanasov <ayatanasov@gmail.com>
2014-10-22 18:09:10 +0300

    added HellBoar

---
+++ RPG-game/Assets/Scripts 1/HellBoar.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HellBoar : Enemy
+{
+
+    // Fields:
+    public static readonly int HellBoarHealth = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        InitEnemy();
+        this.EnemyHealth = HellBoar.HellBoarHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
