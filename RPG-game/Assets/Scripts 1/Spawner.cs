4e5a7a58bc93a41c72c0ee554a3591217591d187
Katya Babacheva <kbabacheva@gmail.com>
2014-10-22 23:07:27 +0300

    spawn

---
+++ RPG-game/Assets/Scripts 1/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+	
+	public GameObject enemy;
+	Vector3 spawnPosition;
+	public float timer= 0.0f;
+	
+	void  spawn_cube (){
+		spawnPosition = new Vector3(0,0,0);
+		Instantiate(enemy,spawnPosition,Quaternion.identity);
+	}
+	
+	void  Update (){
+		timer += Time.deltaTime;
+		if(timer > 10){
+			spawn_cube();
+			timer = 0.0f;
+		}
+	}
+}
+
