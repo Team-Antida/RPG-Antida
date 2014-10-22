dd385c712a99f264bf149e7aed9cc5b65146a1a9
N-Hristov <nicolahristov93@gmail.com>
2014-10-22 21:30:35 +0300

    updated PitKiller.cs

--- RPG-game/Assets/Scripts 1/GameManager.cs
+++ RPG-game/Assets/Scripts 1/GameManager.cs
@@ -117,7 +117,7 @@ public class GameManager : MonoBehaviour
 		}
 	}
 
-	void RestartScene()
+	public void RestartScene()
 	{
 		Application.LoadLevel (Application.loadedLevel);
 	}
--- RPG-game/Assets/Scripts 1/PitKiller.cs
+++ RPG-game/Assets/Scripts 1/PitKiller.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PitKiller : MonoBehaviour {
+public class PitKiller : MonoBehaviour
+{
+    public GameManager gameManager;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Enemy")
+        {
+            gameManager.RestartScene();
+        }
+    }
 }
